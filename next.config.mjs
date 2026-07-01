86e9b02e06a6010d07270bb548ead32484680669
Saenz <alejandro.saenz@tristone.com>
2026-07-01 07:41:52 -0600

    se agrega packagejson

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/dashboard/autorizaciones",
+        destination: "/dashboard/mis_solicitudes",
+        permanent: true,
+      },
+      {
+        source: "/dashboard/solicitudes/mis",
+        destination: "/dashboard/mis_solicitudes",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
